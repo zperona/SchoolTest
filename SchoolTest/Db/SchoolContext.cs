beb7ff7f431d7fc0be45aedfba1b207adda452a5
Zebenzui Perona <zperona@hotmail.com>
2021-06-02 23:07:40 +0200

    add dbcontext and packages

---
+++ SchoolTest/Db/SchoolContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Npgsql;
+using Npgsql.NameTranslation;
+
+
+namespace SchoolTest.Db
+{
+    public class SchoolContext : DbContext
+    {
+        public SchoolContext(DbContextOptions options) :
+           base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+        }
+    }
+}
