b365b2d18bd92283f39a3957ba7ad7873a684935
Zebenzui Perona <zperona@hotmail.com>
2021-06-02 15:16:33 +0200

    initial

---
+++ SchoolTest/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SchoolTest.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
